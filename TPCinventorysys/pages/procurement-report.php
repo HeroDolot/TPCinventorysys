c533c5f877db05bd3d6a2b5785aa1457c3c46e56
HeroDolot <dolot.hero@gmail.com>
2023-04-10 10:08:36 +0800

    =procurement reports

--- TPCinventorysys/pages/procurement-report.php
+++ TPCinventorysys/pages/procurement-report.php
@@ -4,46 +4,42 @@
     include '../components/header.php';
     include '../components/sidebar.php';
     include '../components/navbar.php';
-
-    // $sql = "SELECT * FROM supplier";
-    $result = mysqli_query($conn,$sql);
-
-    echo '<div class="content-wrapper">
-                <div class="content">
-                <div class="container">
-                    <div class="card">
-                        <div class="card-header font-weight-bold">Procurement Details</div>
-                            <div class="card-body">
-                                <table class="table table-striped">
-                                     <thead class="thead-dark">
-                                        <tr>
-                                            <th scope="col">Product Code</th>
-                                            <th scope="col">Item list</th>
-                                            <th scope="col">Quantity</th>
-                                            <th scope="col">Unit Price</th>
-                                            <th scope="col">Total Price</th>
-                                        </tr>
-                                    </thead>
-                                 <tbody>';
-                                 while ($row_once = $result->fetch_assoc()) { 
-                                    echo '<td>'.$row_once['product_code'].'</td>'; 
-                                    echo '<td><select id="item1">'; 
-                                    echo '<option value="'.$row_once["item1"].'">'.$row_once["item1"].'</option>'; 
-                                    while ($row = $result->fetch_assoc()) { 
-                                      echo '<option value="'.$row["item1"].'">'.$row["item1"].'</option>'; 
-                                    } 
-                                    echo '</select></td>'; 
-                                  } 
-    echo '                       </tbody>
-                                 </table>
-                             </div>
-                         </div>
-                     </div>
-                </div>
-            </div>';
-
-
 ?>
+
+<div class="content-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl">
+                <div class="card">
+                    <div class="card-header bg-primary text-white text-xl">Procurement Reports</div>
+                    <div class="card-body">
+                        <table class="table table-striped table-hover text-center text-lg">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>PO 1</td>
+                                    <td><span class="badge badge-danger">On Process</span></td>
+                                    <td><button class="btn btn-info"><i class="fas fa-eye"></i> View</button></td>
+                                </tr>
+                                <tr>
+                                    <td>PO 2</td>
+                                    <td><span class="badge badge-success">Finished</span></td>
+                                    <td><button class="btn btn-info"><i class="fas fa-eye"></i> View</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php 
